86ce50c055b79d5d5640286018929dee4275071d
Govardhan <govardhanemail@example.com>
2026-07-20 06:22:53 +0200

    Added Java microservice files

--- src/main/java/com/govardhan/CICD/UserController.java
+++ src/main/java/com/govardhan/CICD/UserController.java
@@ -1,1 +1,14 @@
+package com.govardhan.CICD;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable int id) {
+        return new User(id, "Govardhan");
+    }
+}
 
